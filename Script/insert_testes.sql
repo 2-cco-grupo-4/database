5f606b2e7d116d61e225f40ee5ec109fbbe8ff6d
DanyloSPTech <danylo.gomes@sptech.school>
2023-05-28 22:05:45 -0300

    feat(views): 2 new views, fix columns names

---
+++ Script/insert_testes.sql
@@ -0,0 +1,190 @@
+INSERT INTO USUARIO (NOME, CPF, EMAIL, SENHA, DATA_NASCIMENTO, AUTENTICADO, CELULAR, DATA_CADASTRO, TIPO_USUARIO, TOKEN_SOLICITACAO)
+VALUES
+  ('João Silva', '12345678901', 'joao@email.com', 'senha123', '1990-01-01', 1, '9999999999', '2023-05-01', 'Cliente', 'token123'),
+  ('Maria Santos', '98765432101', 'maria@email.com', 'senha456', '1995-02-15', 1, '8888888888', '2023-05-02', 'Fotógrafo', 'token456'),
+  ('Pedro Oliveira', '54321678901', 'pedro@email.com', 'senha789', '1988-08-20', 0, '7777777777', '2023-05-03', 'Cliente', NULL),
+  ('Ana Souza', '98765432102', 'ana@email.com', 'senha123', '1992-04-10', 1, '6666666666', '2023-05-04', 'Cliente', 'token789'),
+  ('Lucas Santos', '12345098765', 'lucas@email.com', 'senha456', '1993-11-25', 1, '5555555555', '2023-05-05', 'Fotógrafo', 'tokenabc'),
+  ('Élise Dupont', '111.111.111-11', 'elise.dupont@example.com', 'senha123', '1990-05-15', 1, '(11) 99999-1111', '2023-05-28', 'Cliente', 'abc123'),
+  ('Lucas Lefebvre', '222.222.222-22', 'lucas.lefebvre@example.com', 'senha456', '1995-08-23', 1, '(11) 99999-2222', '2023-05-28', 'Cliente', 'def456'),
+  ('Léa Martin', '333.333.333-33', 'lea.martin@example.com', 'senha789', '1988-03-10', 1, '(11) 99999-3333', '2023-05-28', 'Fotógrafo', 'ghi789'),
+  ('Noémie Dubois', '444.444.444-44', 'noemie.dubois@example.com', 'senhaabc', '1992-11-05', 1, '(11) 99999-4444', '2023-05-28', 'Cliente', 'jklabc'),
+  ('Théo Laurent', '555.555.555-55', 'theo.laurent@example.com', 'senhadef', '1985-07-17', 1, '(11) 99999-5555', '2023-05-28', 'Fotógrafo', 'mnodef'),
+  ('Manon Girard', '666.666.666-66', 'manon.girard@example.com', 'senha123', '1991-02-12', 1, '(11) 99999-6666', '2023-05-28', 'Cliente', 'pqr123'),
+  ('Antoine Moreau', '777.777.777-77', 'antoine.moreau@example.com', 'senha456', '1996-09-27', 1, '(11) 99999-7777', '2023-05-28', 'Cliente', 'stu456'),
+  ('Chloé Rousseau', '888.888.888-88', 'chloe.rousseau@example.com', 'senha789', '1989-04-03', 1, '(11) 99999-8888', '2023-05-28', 'Fotógrafo', 'vwx789'),
+  ('Gabriel Petit', '999.999.999-99', 'gabriel.petit@example.com', 'senhaabc', '1993-12-18', 1, '(11) 99999-9999', '2023-05-28', 'Cliente', 'yz0123'),
+  ('Sophie Lefevre', '000.000.000-00', 'sophie.lefevre@example.com', 'senhadef', '1986-10-08', 1, '(11) 99999-0000', '2023-05-28', 'Fotógrafo', '345def'),
+  ('Hugo Martin', '111.111.111-11', 'hugo.martin@example.com', 'senha123', '1990-05-15', 1, '(11) 99999-1111', '2023-05-28', 'Cliente', '678abc'),
+  ('Camille Roux', '222.222.222-22', 'camille.roux@example.com', 'senha456', '1995-08-23', 1, '(11) 99999-2222', '2023-05-28', 'Cliente', '901def'),
+  ('Juliette Lemoine', '333.333.333-33', 'juliette.lemoine@example.com', 'senha789', '1988-03-10', 1, '(11) 99999-3333', '2023-05-28', 'Fotógrafo', '234ghi'),
+  ('Louis Blanchard', '444.444.444-44', 'louis.blanchard@example.com', 'senhaabc', '1992-11-05', 1, '(11) 99999-4444', '2023-05-28', 'Cliente', '567jkl'),
+  ('Léna Roy', '555.555.555-55', 'lena.roy@example.com', 'senhadef', '1985-07-17', 1, '(11) 99999-5555', '2023-05-28', 'Fotógrafo', '890mno'),
+  ('Thomas David', '666.666.666-66', 'thomas.david@example.com', 'senha123', '1991-02-12', 1, '(11) 99999-6666', '2023-05-28', 'Cliente', '123pqr'),
+  ('Mathilde Lefort', '777.777.777-77', 'mathilde.lefort@example.com', 'senha456', '1996-09-27', 1, '(11) 99999-7777', '2023-05-28', 'Cliente', '456stu'),
+  ('Nathan Guillaume', '888.888.888-88', 'nathan.guillaume@example.com', 'senha789', '1989-04-03', 1, '(11) 99999-8888', '2023-05-28', 'Fotógrafo', '789vwx'),
+  ('Emma Renard', '999.999.999-99', 'emma.renard@example.com', 'senhaabc', '1993-12-18', 1, '(11) 99999-9999', '2023-05-28', 'Cliente', '012yz'),
+  ('Romain Dubois', '000.000.000-00', 'romain.dubois@example.com', 'senhadef', '1986-10-08', 1, '(11) 99999-0000', '2023-05-28', 'Fotógrafo', '345678'),
+  ('Louise Mercier', '111.111.111-11', 'louise.mercier@example.com', 'senha123', '1990-05-15', 1, '(11) 99999-1111', '2023-05-28', 'Cliente', '901234'),
+  ('Pauline Leroy', '222.222.222-22', 'pauline.leroy@example.com', 'senha456', '1995-08-23', 1, '(11) 99999-2222', '2023-05-28', 'Cliente', '567890'),
+  ('Lucie Caron', '333.333.333-33', 'lucie.caron@example.com', 'senha789', '1988-03-10', 1, '(11) 99999-3333', '2023-05-28', 'Fotógrafo', '123abc'),
+  ('Hugo Bernard', '444.444.444-44', 'hugo.bernard@example.com', 'senhaabc', '1992-11-05', 1, '(11) 99999-4444', '2023-05-28', 'Cliente', '456def'),
+  ('Alice Dufour', '555.555.555-55', 'alice.dufour@example.com', 'senhadef', '1985-07-17', 1, '(11) 99999-5555', '2023-05-28', 'Fotógrafo', '789ghi'),
+  ('Jules Renaud', '666.666.666-66', 'jules.renaud@example.com', 'senha123', '1991-02-12', 1, '(11) 99999-6666', '2023-05-28', 'Cliente', '012jkl'),
+  ('Lola Aubry', '777.777.777-77', 'lola.aubry@example.com', 'senha456', '1996-09-27', 1, '(11) 99999-7777', '2023-05-28', 'Cliente', '345mno'),
+  ('Mathis Michel', '888.888.888-88', 'mathis.michel@example.com', 'senha789', '1989-04-03', 1, '(11) 99999-8888', '2023-05-28', 'Fotógrafo', '678pqr'),
+  ('Léonie Dupuis', '999.999.999-99', 'leonie.dupuis@example.com', 'senhaabc', '1993-12-18', 1, '(11) 99999-9999', '2023-05-28', 'Cliente', '901stu'),
+  ('Louis Lecomte', '000.000.000-00', 'louis.lecomte@example.com', 'senhadef', '1986-10-08', 1, '(11) 99999-0000', '2023-05-28', 'Fotógrafo', '234vwx'),
+  ('Inès Brun', '111.111.111-11', 'ines.brun@example.com', 'senha123', '1990-05-15', 1, '(11) 99999-1111', '2023-05-28', 'Cliente', '567yz'),
+  ('Nathan Lambert', '222.222.222-22', 'nathan.lambert@example.com', 'senha456', '1995-08-23', 1, '(11) 99999-2222', '2023-05-28', 'Cliente', '890abc'),
+  ('Lucie Guérin', '333.333.333-33', 'lucie.guerin@example.com', 'senha789', '1988-03-10', 1, '(11) 99999-3333', '2023-05-28', 'Fotógrafo', '123def'),
+  ('Maxime Pierre', '444.444.444-44', 'maxime.pierre@example.com', 'senhaabc', '1992-11-05', 1, '(11) 99999-4444', '2023-05-28', 'Cliente', '456ghi'),
+  ('Léna Renard', '555.555.555-55', 'lena.renard@example.com', 'senhadef', '1985-07-17', 1, '(11) 99999-5555', '2023-05-28', 'Fotógrafo', '789jkl'),
+  ('Mathis Dupont', '666.666.666-66', 'mathis.dupont@example.com', 'senha123', '1991-02-12', 1, '(11) 99999-6666', '2023-05-28', 'Cliente', '012mno'),
+  ('Lola Lemoine', '777.777.777-77', 'lola.lemoine@example.com', 'senha456', '1996-09-27', 1, '(11) 99999-7777', '2023-05-28', 'Cliente', '345pqr'),
+  ('Tom Lefevre', '888.888.888-88', 'tom.lefevre@example.com', 'senha789', '1989-04-03', 1, '(11) 99999-8888', '2023-05-28', 'Fotógrafo', '678stu'),
+  ('Louise Simon', '999.999.999-99', 'louise.simon@example.com', 'senhaabc', '1993-12-18', 1, '(11) 99999-9999', '2023-05-28', 'Cliente', '901vwx'),
+  ('Arthur Petit', '000.000.000-00', 'arthur.petit@example.com', 'senhadef', '1986-10-08', 1, '(11) 99999-0000', '2023-05-28', 'Fotógrafo', '234yz');
+
+INSERT INTO TEMA (NOME)
+VALUES
+  ('Retratos'),
+  ('Natureza'),
+  ('Casamentos'),
+  ('Paisagens'),
+  ('Animais');
+
+
+INSERT INTO USUARIO_TEMAS (ID_USUARIO, ID_TEMA)
+VALUES
+  (1, 1),
+  (1, 3),
+  (2, 2),
+  (2, 4),
+  (3, 3),
+  (3, 5),
+  (4, 1),
+  (5, 2),
+  (5, 4);
+
+
+INSERT INTO EVENTO (DATA_REALIZACAO, STATUS_EVENTO, FK_FOTOGRAFO, FK_TEMA, FK_CLIENTE, VALOR)
+VALUES
+  ('2023-06-10', 'Confirmado', 2, 3, 1, 500.00),
+  ('2023-07-05', 'Pendente', 2, 1, 3, 350.00),
+  ('2023-08-20', 'Confirmado', 5, 2, 4, 700.00),
+  ('2023-09-15', 'Pendente', 5, 4, 2, 400.00),
+  ('2023-10-12', 'Cancelado', 2, 5, 5, 250.00),
+  ('2023-06-20', 'Proposta', 2, 1, 3, 400.00),
+  ('2023-06-25', 'Negociando', 4, 2, 1, 600.00),
+  ('2023-07-01', 'Confirmado', 5, 3, 2, 800.00),
+  ('2023-07-05', 'Cancelado', 3, 4, 5, 300.00),
+  ('2023-07-10', 'Finalizado', 2, 5, 4, 700.00),
+  ('2023-07-15', 'Proposta', 1, 1, 3, 450.00),
+  ('2023-07-20', 'Negociando', 4, 2, 5, 550.00),
+  ('2023-07-25', 'Confirmado', 2, 3, 1, 900.00),
+  ('2023-07-30', 'Cancelado', 5, 4, 2, 350.00),
+  ('2023-08-05', 'Finalizado', 3, 5, 4, 750.00),
+  ('2023-08-10', 'Proposta', 1, 1, 3, 400.00),
+  ('2023-08-15', 'Negociando', 5, 2, 4, 550.00),
+  ('2023-08-20', 'Confirmado', 2, 3, 1, 800.00),
+  ('2023-08-25', 'Cancelado', 4, 4, 5, 300.00),
+  ('2023-08-30', 'Finalizado', 3, 5, 2, 700.00),
+  ('2023-09-05', 'Proposta', 1, 1, 3, 450.00),
+  ('2023-09-10', 'Negociando', 4, 2, 2, 600.00),
+  ('2023-09-15', 'Confirmado', 5, 3, 1, 850.00),
+  ('2023-09-20', 'Cancelado', 2, 4, 4, 350.00),
+  ('2023-09-25', 'Finalizado', 3, 5, 5, 800.00),
+  ('2023-10-01', 'Proposta', 1, 1, 3, 400.00),
+  ('2023-10-05', 'Negociando', 4, 2, 1, 550.00),
+  ('2023-10-10', 'Confirmado', 2, 3, 2, 900.00),
+  ('2023-10-15', 'Cancelado', 5, 4, 3, 300.00),
+  ('2023-10-20', 'Finalizado', 3, 5, 4, 750.00),
+  ('2023-10-25', 'Proposta', 1, 1, 2, 400.00),
+  ('2023-10-30', 'Negociando', 5, 2, 4, 600.00),
+  ('2023-11-05', 'Confirmado', 2, 3, 1, 850.00),
+  ('2023-11-10', 'Cancelado', 4, 4, 3, 350.00),
+  ('2023-11-15', 'Finalizado', 3, 5, 5, 800.00),
+  ('2023-11-20', 'Proposta', 1, 1, 2, 450.00),
+  ('2023-11-25', 'Negociando', 4, 2, 1, 600.00),
+  ('2023-11-30', 'Confirmado', 2, 3, 3, 900.00),
+  ('2023-12-05', 'Cancelado', 5, 4, 2, 350.00),
+  ('2023-12-10', 'Finalizado', 3, 5, 1, 750.00),
+  ('2023-12-15', 'Proposta', 1, 1, 4, 400.00),
+  ('2023-12-20', 'Negociando', 4, 2, 5, 550.00),
+  ('2023-12-25', 'Confirmado', 2, 3, 2, 800.00),
+  ('2023-12-30', 'Cancelado', 5, 4, 1, 300.00),
+  ('2024-01-05', 'Finalizado', 3, 5, 3, 700.00),
+  ('2024-01-10', 'Proposta', 1, 1, 5, 450.00),
+  ('2024-01-15', 'Negociando', 5, 2, 2, 600.00);
+
+
+INSERT INTO ENDERECO (ESTADO, CIDADE, CEP, BAIRRO, LOGRADOURO, NUMERO, COMPLEMENTO, FK_EVENTO)
+VALUES
+  ('São Paulo', 'São Paulo', '01234-567', 'Centro', 'Rua A', '123', 'Apto 4', 1),
+  ('Rio de Janeiro', 'Rio de Janeiro', '98765-432', 'Copacabana', 'Avenida B', '456', NULL, 2),
+  ('São Paulo', 'São Paulo', '01234-567', 'Centro', 'Rua C', '789', 'Casa 2', 3),
+  ('Rio de Janeiro', 'Rio de Janeiro', '98765-432', 'Ipanema', 'Avenida D', '1011', NULL, 4),
+  ('São Paulo', 'São Paulo', '01234-567', 'Centro', 'Rua E', '1213', 'Apto 5', 5);
+
+
+INSERT INTO ALBUM (TITULO, DESCRICAO, FK_FOTOGRAFO, FK_TEMA)
+VALUES
+  ('Álbum 1', 'Fotos do evento 1', 2, 3),
+  ('Álbum 2', 'Fotos do evento 2', 2, 1),
+  ('Álbum 3', 'Fotos do evento 3', 5, 2),
+  ('Álbum 4', 'Fotos do evento 4', 5, 4),
+  ('Álbum 5', 'Fotos do evento 5', 2, 5);
+
+
+INSERT INTO IMAGEM (CAMINHO, DESCRICAO, TIPO, FK_ALBUM)
+VALUES
+  ('caminho1.jpg', 'Foto 1', 'jpg', 1),
+  ('caminho2.jpg', 'Foto 2', 'jpg', 1),
+  ('caminho3.jpg', 'Foto 3', 'jpg', 2),
+  ('caminho4.jpg', 'Foto 4', 'jpg', 3),
+  ('caminho5.jpg', 'Foto 5', 'jpg', 4),
+  ('caminho6.jpg', 'Foto 6', 'jpg', 5);
+
+
+INSERT INTO TAG (ID_TAG, NOME)
+VALUES
+  (1, 'Casamento'),
+  (2, 'Retrato'),
+  (3, 'Natureza'),
+  (4, 'Animais'),
+  (5, 'Paisagem');
+
+
+
+INSERT INTO TAG_IMAGEM (ID_IMAGEM, ID_TAG)
+VALUES
+  (1, 1),
+  (2, 2),
+  (3, 3),
+  (4, 4),
+  (5, 5),
+  (6, 1),
+  (6, 2);
+
+
+INSERT INTO INTERESSE_TAG (ID_TAG, ID_USUARIO)
+VALUES
+  (1, 1),
+  (2, 1),
+  (2, 2),
+  (3, 3),
+  (3, 5),
+  (4, 4),
+  (4, 5),
+  (5, 1),
+  (5, 2);
+
+
+INSERT INTO LOG_PAGINA_SITE (ID_LOG_PAGINA_SITE, SESSAO_ATIVA, TEMPO_DURACAO_SESSAO, DATA_SESSAO, QTD_NOVA_SESSAO, TOTAL_VIEW_PAGINA, LOG_PAGINA_SITECOL)
+VALUES
+  (1, 1, 120.5, '2023-05-01', 5, 10, 'Página 1'),
+  (2, 0, 60.2, '2023-05-02', 3, 8, 'Página 2'),
+  (3, 1, 180.8, '2023-05-03', 7, 15, 'Página 1'),
+  (4, 0, 90.3, '2023-05-04', 4, 12, 'Página 3'),
+  (5, 1, 150.0, '2023-05-05', 6, 11, 'Página 2');
