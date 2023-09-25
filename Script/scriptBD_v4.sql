ca043442b089923151b343786627447269d7f632
DanyloSPTech <danylo.gomes@sptech.school>
2023-09-25 00:58:19 -0300

    feat(PIC-38): fix script tables and update views admin

--- Script/scriptBD_v4.sql
+++ Script/scriptBD_v4.sql
@@ -3,138 +3,141 @@ CREATE DATABASE picme;
 USE picme;
 
 CREATE TABLE tb_usuario (
-  id_usuario INT PRIMARY KEY,
+  id_usuario BIGINT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(120),
   cpf CHAR(14),
-  email VARCHAR(120),
+  email VARCHAR(256),
   senha VARCHAR(100),
-  dataNascimento DATE,
-  celular VARCHAR(15),
-  data_cadastro DATE,
+  data_nascimento DATE,
+  celular VARCHAR(20),
+  data_cadastro DATETIME,
   tipo_usuario TINYINT,
-  token_solicitacao VARCHAR(150),
+  token_solicitacao VARCHAR(200),
   cidade_preferencia VARCHAR(150),
   estado_preferencia VARCHAR(150),
-  autenticado TINYINT
+  autenticado TINYINT,
+  INDEX(cpf, email)
 );
 
 CREATE TABLE tb_tag (
-  id_tag INT PRIMARY KEY,
+  id_tag BIGINT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(100)
 );
 
 CREATE TABLE tb_tema (
-  id_tema INT PRIMARY KEY,
+  id_tema BIGINT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(50)
 );
 
 CREATE TABLE tb_usuario_tag (
-  id_tag INT,
-  id_usuario INT,
+  id_tag BIGINT,
+  id_usuario BIGINT,
+  PRIMARY KEY (id_tag, id_usuario),
   FOREIGN KEY (id_tag) REFERENCES tb_tag(id_tag),
   FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
 CREATE TABLE tb_album (
-  id_album INT PRIMARY KEY,
+  id_album BIGINT PRIMARY KEY AUTO_INCREMENT,
   titulo VARCHAR(100),
   descricao VARCHAR(100),
-  fk_fotografo INT,
-  fk_tema INT,
+  fk_fotografo BIGINT,
+  fk_tema BIGINT,
   FOREIGN KEY (fk_fotografo) REFERENCES tb_usuario(id_usuario),
   FOREIGN KEY (fk_tema) REFERENCES tb_tema(id_tema)
 );
 
 CREATE TABLE tb_log_acessos (
-  id_log_acessos INT PRIMARY KEY,
-  data_login DATE,
-  fk_usuario INT,
+  id_log_acessos BIGINT PRIMARY KEY AUTO_INCREMENT,
+  data_login DATETIME,
+  fk_usuario BIGINT,
   FOREIGN KEY (fk_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
 CREATE TABLE tb_log_pesquisas (
-  id_log_pesquisas INT PRIMARY KEY,
+  id_log_pesquisas BIGINT PRIMARY KEY AUTO_INCREMENT,
   termo_busca VARCHAR(150),
-  data_pesquisa DATE,
-  fk_usuario INT,
+  data_pesquisa DATETIME,
+  fk_usuario BIGINT,
   FOREIGN KEY (fk_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
 CREATE TABLE tb_imagem (
-  id_imagem INT PRIMARY KEY,
+  id_imagem BIGINT PRIMARY KEY AUTO_INCREMENT,
   media_url VARCHAR(500),
   permalink VARCHAR(500),
   caption VARCHAR(100),
   media_type VARCHAR(5),
-  updated_at DATE,
-  fk_album INT,
+  updated_at DATETIME,
+  fk_album BIGINT,
   FOREIGN KEY (fk_album) REFERENCES tb_album(id_album)
 );
 
 CREATE TABLE tb_tag_imagem (
-  id_imagem INT,
-  id_tag INT,
+  id_imagem BIGINT,
+  id_tag BIGINT,
+  PRIMARY KEY (id_imagem, id_tag),
   FOREIGN KEY (id_imagem) REFERENCES tb_imagem(id_imagem),
   FOREIGN KEY (id_tag) REFERENCES tb_tag(id_tag)
 );
 
-
 CREATE TABLE tb_usuario_tema (
-  id_tema INT,
-  id_usuario INT,
+  id_tema BIGINT,
+  id_usuario BIGINT,
+  PRIMARY KEY (id_tema, id_usuario),
   FOREIGN KEY (id_tema) REFERENCES tb_tema(id_tema),
   FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
 CREATE TABLE tb_sessao (
-  id_sessao INT PRIMARY KEY,
-  data_realizacao DATE,
+  id_sessao BIGINT PRIMARY KEY AUTO_INCREMENT,
+  data_realizacao DATETIME,
   status_sessao VARCHAR(20),
-  fk_fotografo INT,
-  fk_tema INT,
-  fk_cliente INT,
+  fk_fotografo BIGINT,
+  fk_tema BIGINT,
+  fk_cliente BIGINT,
+  created_at DATETIME,
   FOREIGN KEY (fk_fotografo) REFERENCES tb_usuario(id_usuario),
   FOREIGN KEY (fk_tema) REFERENCES tb_tema(id_tema),
   FOREIGN KEY (fk_cliente) REFERENCES tb_usuario(id_usuario)
 );
 
 CREATE TABLE tb_endereco (
-  id_endereco INT PRIMARY KEY,
+  id_endereco BIGINT PRIMARY KEY AUTO_INCREMENT,
   estado VARCHAR(150),
   cidade VARCHAR(150),
-  cep VARCHAR(80),
-  bairro VARCHAR(10),
+  cep VARCHAR(10),
+  bairro VARCHAR(80),
   logradouro VARCHAR(100),
   numero VARCHAR(10),
   complemento VARCHAR(30),
-  fk_sessao INT,
+  fk_sessao BIGINT,
   FOREIGN KEY (fk_sessao) REFERENCES tb_sessao(id_sessao)
 );
 
 CREATE TABLE tb_pagamento (
-  id_pagamento INT PRIMARY KEY,
+  id_pagamento BIGINT PRIMARY KEY AUTO_INCREMENT,
   forma VARCHAR(60),
   valor DECIMAL,
   parcelas INT,
-  fk_sessao INT,
+  fk_sessao BIGINT,
   FOREIGN KEY (fk_sessao) REFERENCES tb_sessao(id_sessao)
 );
 
 CREATE TABLE tb_avaliacao (
-  id_avaliacao INT PRIMARY KEY,
+  id_avaliacao BIGINT PRIMARY KEY AUTO_INCREMENT,
   descricao VARCHAR(300),
   nota DOUBLE,
-  fk_sessao INT,
-  FOREIGN KEY (fk_sessao) REFERENCES tb_sessao(id_sessao)
+  fk_sessao BIGINT,
+  FOREIGN KEY (fk_sessao) REFERENCES tb_sessao(id_sessao),
+  INDEX (nota)
 );
 
 CREATE TABLE tb_log_sessao (
-  id_log_sessao INT PRIMARY KEY,
-  data_modificacao DATE,
+  id_log_sessao BIGINT PRIMARY KEY AUTO_INCREMENT,
+  data_modificacao DATETIME,
   status_sessao VARCHAR(20),
-  data_realizacao DATE,
-  fk_sessao INT,
+  data_realizacao DATETIME,
+  fk_sessao BIGINT,
   FOREIGN KEY (fk_sessao) REFERENCES tb_sessao(id_sessao)
 );
-
-
---
+++ Script/views-admin.sql
@@ -0,0 +1,174 @@
+SET lc_time_names = 'pt_BR';
+
+USE PICME;
+
+SELECT * FROM VW_CLIENTES_IMEDIATOS_MES VCIM;
+
+-- View retorna quantos clientes 'imediatos' tivemos do mês, ou seja, quando clientes que se cadastraram e em menos de 1 semana criaram uma sessão
+CREATE VIEW vw_clientes_imediatos_mes
+AS
+	SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 0 MONTH)) AS 'Mes',
+		(SELECT COUNT(DISTINCT id_usuario) FROM (SELECT id_usuario FROM tb_usuario INNER JOIN tb_sessao ON tb_usuario.id_usuario = tb_sessao.fk_cliente WHERE DATEDIFF(tb_usuario.data_cadastro, tb_sessao.created_at) <= 7 AND tipo_usuario LIKE 1 AND MONTHNAME(tb_sessao.created_at) LIKE MONTHNAME(DATE_SUB(NOW(), INTERVAL 0 MONTH))) AS ClientesImediatos) AS 'Agendaram',
+        (SELECT COUNT(DISTINCT id_usuario) FROM tb_usuario WHERE MONTH(tb_usuario.data_cadastro) = MONTH(NOW()) AND tipo_usuario = 1) AS 'Total',
+        (SELECT Total - Agendaram) AS 'NaoAgendaram'
+     UNION
+     SELECT
+      	MONTHNAME(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS 'Mes',
+		(SELECT COUNT(DISTINCT id_usuario) FROM (SELECT id_usuario FROM tb_usuario INNER JOIN tb_sessao ON tb_usuario.id_usuario = tb_sessao.fk_cliente WHERE DATEDIFF(tb_usuario.data_cadastro, tb_sessao.created_at) <= 7 AND tipo_usuario LIKE 1 AND MONTHNAME(tb_sessao.created_at) LIKE MONTHNAME(DATE_SUB(NOW(), INTERVAL 1 MONTH))) AS ClientesImediatos) AS 'Agendaram',
+        (SELECT COUNT(DISTINCT id_usuario) FROM tb_usuario WHERE MONTH(tb_usuario.data_cadastro) = (MONTH(NOW()) - 1) AND tipo_usuario = 1) AS 'Total',
+        (SELECT Total - Agendaram) AS 'NaoAgendaram'
+     UNION
+     SELECT
+      	MONTHNAME(DATE_SUB(NOW(), INTERVAL 2 MONTH)) AS 'Mes',
+		(SELECT COUNT(DISTINCT id_usuario) FROM (SELECT id_usuario FROM tb_usuario INNER JOIN tb_sessao ON tb_usuario.id_usuario = tb_sessao.fk_cliente WHERE DATEDIFF(tb_usuario.data_cadastro, tb_sessao.created_at) <= 7 AND tipo_usuario LIKE 1 AND MONTHNAME(tb_sessao.created_at) LIKE MONTHNAME(DATE_SUB(NOW(), INTERVAL 2 MONTH))) AS ClientesImediatos) AS 'Agendaram',
+        (SELECT COUNT(DISTINCT id_usuario) FROM tb_usuario WHERE MONTH(tb_usuario.data_cadastro) = (MONTH(NOW()) - 2) AND tipo_usuario = 1) AS 'Total',
+       	(SELECT Total - Agendaram) AS 'NaoAgendaram'
+     UNION
+     SELECT
+      	MONTHNAME(DATE_SUB(NOW(), INTERVAL 3 MONTH)) AS 'Mes',
+		(SELECT COUNT(DISTINCT id_usuario) FROM (SELECT id_usuario FROM tb_usuario INNER JOIN tb_sessao ON tb_usuario.id_usuario = tb_sessao.fk_cliente WHERE DATEDIFF(tb_usuario.data_cadastro, tb_sessao.created_at) <= 7 AND tipo_usuario LIKE 1 AND MONTHNAME(tb_sessao.created_at) LIKE MONTHNAME(DATE_SUB(NOW(), INTERVAL 3 MONTH))) AS ClientesImediatos) AS 'Agendaram',
+        (SELECT COUNT(DISTINCT id_usuario) FROM tb_usuario WHERE MONTH(tb_usuario.data_cadastro) = (MONTH(NOW()) - 3) AND tipo_usuario = 1) AS 'Total',
+        (SELECT Total - Agendaram) AS 'NaoAgendaram'
+     UNION
+     SELECT
+      	MONTHNAME(DATE_SUB(NOW(), INTERVAL 4 MONTH)) AS 'Mes',
+		(SELECT COUNT(DISTINCT id_usuario) FROM (SELECT id_usuario FROM tb_usuario INNER JOIN tb_sessao ON tb_usuario.id_usuario = tb_sessao.fk_cliente WHERE DATEDIFF(tb_usuario.data_cadastro, tb_sessao.created_at) <= 7 AND tipo_usuario LIKE 1 AND MONTHNAME(tb_sessao.created_at) LIKE MONTHNAME(DATE_SUB(NOW(), INTERVAL 4 MONTH))) AS ClientesImediatos) AS 'Agendaram',
+        (SELECT COUNT(DISTINCT id_usuario) FROM tb_usuario WHERE MONTH(tb_usuario.data_cadastro) = (MONTH(NOW()) - 4) AND tipo_usuario = 1) AS 'Total',
+        (SELECT Total - Agendaram) AS 'NaoAgendaram';
+       
+-- View retorna contagem de quantos contatos foram iniciados para cada tema cadastrado 
+CREATE VIEW vw_tema_count_sessoes
+AS
+	SELECT
+		nome AS Tema,
+		COUNT(fk_tema) AS Sessoes
+	FROM
+		tb_sessao
+	INNER JOIN
+		tb_tema ON tb_sessao.fk_tema = tb_tema.id_tema
+	GROUP BY
+		Tema;
+
+        
+-- View que retorna contagem de usuario do tipo Cliente separados por faixa etária
+CREATE VIEW vw_faixa_etaria_cliente
+AS
+	SELECT
+		'18-20' AS Faixa,
+			(SELECT COUNT(id_usuario) FROM tb_usuario WHERE TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) > 18 AND TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) <= 20 AND tipo_usuario = 1) AS Quantidade
+	UNION
+	SELECT
+		'21-25' AS Faixa,
+			(SELECT COUNT(id_usuario) FROM tb_usuario WHERE TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) > 20 AND TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) <= 25 AND tipo_usuario = 1) AS Quantidade
+	UNION
+	SELECT
+		'26-30' AS Faixa,
+			(SELECT COUNT(id_usuario) FROM tb_usuario WHERE TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) > 25 AND TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) <= 30 AND tipo_usuario = 1) AS Quantidade
+	UNION
+	SELECT
+		'31-35' AS Faixa,
+		(SELECT COUNT(id_usuario) FROM tb_usuario WHERE TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) > 30 AND TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) <= 35 AND tipo_usuario = 1) AS Quantidade
+	UNION
+	SELECT
+		'36-40' AS Faixa,
+		(SELECT COUNT(id_usuario) FROM tb_usuario WHERE TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) > 35 AND TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) <= 40 AND tipo_usuario = 1) AS Quantidade
+	UNION
+	SELECT
+		'41+' AS Faixa,
+		(SELECT COUNT(id_usuario) FROM tb_usuario WHERE TIMESTAMPDIFF(YEAR, data_nascimento, CURDATE()) > 40 AND tipo_usuario = 1) AS Quantidade;
+        
+
+-- View retorna quantos clientes são fotógrafos e quantos são clientes
+CREATE VIEW vw_total_clientes_fotografos
+AS
+	SELECT
+		'Clientes' as Label, (SELECT COUNT(id_usuario) FROM usuario WHERE tipo_usuario = 1) AS 'Quantidade'
+	UNION
+    SELECT
+        'Fotógrafos' as Label, (SELECT COUNT(id_usuario) FROM usuario WHERE tipo_usuario = 2) AS 'Quantidade';
+        
+-- View retorna quantas sessões foram finalizadas e quantas foram canceladas
+CREATE VIEW vw_total_sessoes_finalizadas_canceladas
+AS
+	SELECT
+		'Convertidos' as Label, (SELECT COUNT(id_evento) FROM evento WHERE status_evento LIKE 'Finalizado') AS 'Quantidade'
+	UNION
+    SELECT
+        'Interrompidos' as Label, (SELECT COUNT(id_evento) FROM evento WHERE status_evento LIKE 'Cancelado') AS 'Quantidade';
+        
+-- View que retorna a progressão da quantidade de usuários cadastrados nos últimos 6 meses
+CREATE VIEW vw_progressao_cadastro_usuarios
+AS
+	SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 6 MONTH)) AS 'Mes', COUNT(id_usuario) AS 'Quantidade' FROM usuario WHERE TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) = 6
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 5 MONTH)) AS 'Mes', COUNT(id_usuario) AS 'Quantidade' FROM usuario WHERE TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) = 5
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 4 MONTH)) AS 'Mes', COUNT(id_usuario) AS 'Quantidade' FROM usuario WHERE TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) = 4
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 3 MONTH)) AS 'Mes', COUNT(id_usuario) AS 'Quantidade' FROM usuario WHERE TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) = 3
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 2 MONTH)) AS 'Mes', COUNT(id_usuario) AS 'Quantidade' FROM usuario WHERE TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) = 2
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS 'Mes', COUNT(id_usuario) AS 'Quantidade' FROM usuario WHERE TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) = 1
+	UNION
+	SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 0 MONTH)) AS 'Mes', COUNT(id_usuario) AS 'Quantidade' FROM usuario WHERE TIMESTAMPDIFF(MONTH, data_cadastro, CURDATE()) = 0;
+        
+-- View que retorna a progressão da quantidade de eventos concluidos
+CREATE VIEW vw_progressao_sessoes_realizadas
+AS
+	SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 6 MONTH)) AS 'Mes', COUNT(id_evento) AS 'Quantidade' FROM evento WHERE TIMESTAMPDIFF(MONTH, data_realizacao, CURDATE()) = 6 AND status_evento = 'Finalizado'
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 5 MONTH)) AS 'Mes', COUNT(id_evento) AS 'Quantidade' FROM evento WHERE TIMESTAMPDIFF(MONTH, data_realizacao, CURDATE()) = 5 AND status_evento = 'Finalizado'
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 4 MONTH)) AS 'Mes', COUNT(id_evento) AS 'Quantidade' FROM evento WHERE TIMESTAMPDIFF(MONTH, data_realizacao, CURDATE()) = 4 AND status_evento = 'Finalizado'
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 3 MONTH)) AS 'Mes', COUNT(id_evento) AS 'Quantidade' FROM evento WHERE TIMESTAMPDIFF(MONTH, data_realizacao, CURDATE()) = 3 AND status_evento = 'Finalizado'
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 2 MONTH)) AS 'Mes', COUNT(id_evento) AS 'Quantidade' FROM evento WHERE TIMESTAMPDIFF(MONTH, data_realizacao, CURDATE()) = 2 AND status_evento = 'Finalizado'
+	UNION
+    SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS 'Mes', COUNT(id_evento) AS 'Quantidade' FROM evento WHERE TIMESTAMPDIFF(MONTH, data_realizacao, CURDATE()) = 1 AND status_evento = 'Finalizado'
+	UNION
+	SELECT
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 0 MONTH)) AS 'Mes', COUNT(id_evento) AS 'Quantidade' FROM evento WHERE TIMESTAMPDIFF(MONTH, data_realizacao, CURDATE()) = 0 AND status_evento = 'Finalizado';
+        
+-- View KPI 1 - Usuários cadastros no mês atual e no último mês
+CREATE VIEW vw_kpi_usuarios_mes
+AS 
+    SELECT 
+		MONTHNAME(NOW()) AS 'Mes', (SELECT COUNT(id_usuario) FROM tb_usuario WHERE DATEDIFF(CURDATE(), data_cadastro) <= 31) AS 'Quantidade'
+	UNION
+    SELECT 
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS 'Mes', (SELECT COUNT(id_usuario) FROM tb_usuario WHERE DATEDIFF(CURDATE(), data_cadastro) > 31 AND DATEDIFF(CURDATE(), data_cadastro) <= 62) AS 'MesPassado';
+        
+-- View KPI 2 - Sessões realizadas no mês atual e no último mês     
+CREATE VIEW vw_kpi_sessoes_mes
+AS
+    SELECT 
+		MONTHNAME(NOW()) AS 'Mes', (SELECT COUNT(id_sessao) FROM tb_sessao WHERE status_sessao = 'Finalizado' AND DATEDIFF(CURDATE(), data_realizacao) <= 31) AS 'Sessoes'
+	UNION
+    SELECT 
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS 'Mes', (SELECT COUNT(id_sessao) FROM tb_sessao WHERE status_sessao = 'Finalizado' AND DATEDIFF(CURDATE(), data_realizacao) > 31 AND DATEDIFF(CURDATE(), data_realizacao) <= 62) AS 'Sessoes';
+
+-- View KPI 3 - Acessos no mês atual e no último mês         
+CREATE VIEW vw_kpi_acessos_mes
+AS
+    SELECT 
+		MONTHNAME(NOW()) AS 'Mes', (SELECT COUNT(fk_usuario) FROM tb_log_acessos WHERE DATEDIFF(CURDATE(), data_login) <= 31) AS 'Acessos'
+	UNION
+    SELECT 
+		MONTHNAME(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AS 'Mes', (SELECT COUNT(fk_usuario) FROM tb_log_acessos WHERE DATEDIFF(CURDATE(), data_login) > 31 AND DATEDIFF(CURDATE(), data_login) <= 62) AS 'Acessos';
+	
